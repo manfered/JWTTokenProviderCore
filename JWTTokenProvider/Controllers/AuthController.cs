d79824115905138826b6b2c45aad78de953e323c
manfered <farzad.seifi@gmail.com>
2019-02-06 16:20:58 -0700

    Add project files.

---
+++ JWTTokenProvider/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using JWTTokenProvider.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTTokenProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public AuthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // POST api/Auth/Token
+        [HttpPost("token")]
+        public ActionResult Token(TokenModelInPut inputModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // search for the username and password
+            if (inputModel.Username == "username" && inputModel.Password == "pass")
+            {
+                var claimData = new[]
+                {
+                    new Claim(ClaimTypes.Name, inputModel.Username)
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTSecurityKey"]));
+                var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                //var signingCredentials2 = new SigningCredentials(new RsaSecurityKey(new RSACryptoServiceProvider(2048).ExportParameters(true)), SecurityAlgorithms.RsaSha256Signature);
+                var expiryDate = DateTime.UtcNow.AddDays(30);
+                var token = new JwtSecurityToken(
+                        issuer: "mysite.com",
+                        audience: "mysite.com",
+                        expires: expiryDate,
+                        claims: claimData,
+                        signingCredentials: signingCredentials
+                    );
+
+                TokenModelOutPut tokenModel = new TokenModelOutPut()
+                {
+                    AccessToken = new JwtSecurityTokenHandler().WriteToken(token),
+                    ExpiryDate = expiryDate
+                };
+
+                return Ok(tokenModel);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ JWTTokenProvider/Models/TokenModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTTokenProvider.Models
+{
+    public class TokenModelInPut
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class TokenModelOutPut
+    {
+        public string AccessToken { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
